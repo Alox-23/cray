258e9820a8acf21f001514bc37eefe1cb5514a80
Alox-23 <akuznetsov23000@gmail.com>
2025-05-24 21:46:24 +0200

    made player look cooller hehe

--- renderer.c
+++ renderer.c
@@ -28,13 +28,13 @@ void render_player_2d(Renderer *renderer, Player *player){
  
   SDL_SetRenderDrawColor(renderer->sdl_renderer, 0, 0, 255, 255);
   SDL_Rect rect;
-  rect.x = player->pos.x -10;
-  rect.y = player->pos.y -10;
-  rect.w = 20;
-  rect.h = 20;
+  rect.x = player->pos.x -5;
+  rect.y = player->pos.y -5;
+  rect.w = 10;
+  rect.h = 10;
   SDL_RenderDrawRect(renderer->sdl_renderer, &rect); 
   
-  int scaler = 25;
+  int scaler = 10;
   SDL_RenderDrawLine(renderer->sdl_renderer, player->pos.x, player->pos.y, player->pos.x + player->dir.x * scaler, player->pos.y + player->dir.y * scaler);
 }
 
